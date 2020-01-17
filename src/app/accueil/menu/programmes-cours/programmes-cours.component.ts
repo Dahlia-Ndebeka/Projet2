5ff33bff2f0a3b4662da88a3570ea77300daf102
Dahlia-Ndebeka <honondebeka@gmail.com>
2020-01-17 12:53:24 +0100

    Ajout des components accueil et ceux du menu

---
+++ src/app/accueil/menu/programmes-cours/programmes-cours.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-programmes-cours',
+  templateUrl: './programmes-cours.component.html',
+  styleUrls: ['./programmes-cours.component.scss']
+})
+export class ProgrammesCoursComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { MenuComponent } from './Accueil/menu/menu.component';
+import { ProgrammesCoursComponent } from './Accueil/Menu/programmes-cours/programmes-cours.component';
+import { PresentationComponent } from './Accueil/Menu/presentation/presentation.component';
+import { ContactComponent } from './Accueil/Menu/contact/contact.component';
+import { InscriptionComponent } from './Accueil/Menu/inscription/inscription.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    MenuComponent,
+    ProgrammesCoursComponent,
+    PresentationComponent,
+    ContactComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule,
